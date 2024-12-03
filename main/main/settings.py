bbe3518599c2a62156da76619c064e5ce28a1de1
hamzakhalem <hamzakhalem97@gmail.com>
2024-12-03 09:26:52 +0100

    fix: zip place for availability_sectioncompleted

--- main/main/settings.py
+++ main/main/settings.py
@@ -38,12 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authetoken',
+    'corsheaders',
+    'djoser',
     'api'
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000'
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheader.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
