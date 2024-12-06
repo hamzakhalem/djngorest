2eaaa2f70134aa5fe7e23adbf511b189d4e07185
hamzakhalem <hamzakhalem97@gmail.com>
2024-12-06 19:44:08 +0100

    fix: zip place for availability_sectioncompleted

--- main/main/settings.py
+++ main/main/settings.py
@@ -130,4 +130,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
