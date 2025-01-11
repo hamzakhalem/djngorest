a8a7b627b27960f427642532b34700165a18a181
hamzakhalem <hamzakhalem97@gmail.com>
2025-01-11 17:23:47 +0100

    fix:bugs related to report builder

--- main/main/asgi.py
+++ main/main/asgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 
+
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
